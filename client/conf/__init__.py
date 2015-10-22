7777cba189e8125a2af39633d0d4639498e8ee99
HongYuanLi1989 <917689356@qq.com>
2015-10-22 12:54:07 +0800

    chang file directory

---
+++ client/conf/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@version: ??
+@author: LiHongYuan
+@contact: 917689356@qq.com
+@software: PyCharm
+@file: __init__.py
+@time: 15-10-20 上午11:08
+"""
+
+
